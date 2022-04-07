931dbc5cb4e4235428902df34fcd792186306370
Florencia Urrejola Ferrer <florurre@gmail.com>
2022-04-07 20:34:22 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+ 
+
+    <div >
+      <HelloWorld />
+    </div>
+  
+</template>
+
+<style>
+@import './assets/base.css';
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+
+<template>
+  <div class="container-nav" >
+    <nav class="nav">
+      <img src="" alt="logo">
+        <ul>
+          <li>PROFESOR</li>
+          <li>PLANIFICACION DIARIA</li>
+          <li>EJERCICIOS</li>
+          <li>ENTREVISTAS</li>
+          <li>DONA AQUI</li>
+          <li>CREAR CUENTA</li>
+        </ul>
+    </nav>
+  </div>
+</template>
+
+<style>
+
+.nav{
+  display: flex;
+  background: black;
+  height: 80px;
+  align-items: center;
+  justify-content: flex-end
+}
+
+.nav ul {
+  display:flex;
+  flex-direction: row;
+  gap: 20px;
+  justify-content: right;
+}
+
+.nav ul li {
+  color: #ffff;
+  list-style: none;
+}
+
+</style>
+
+    <script setup>
+    defineProps({
+      msg: {
+        type: String,
+        required: true
+      }
+    })
+    </script>
